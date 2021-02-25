9188d38ddf1eb52c8090bab8c208e0030ca1df74
Sam Bedwell <sam@wordsfordreaming.com>
2021-02-25 16:45:18 -0500

    Add comments clarifying the keys in the config file

--- config/fixtures.php
+++ config/fixtures.php
@@ -1,8 +1,15 @@
 <?php
 
 return [
+    /**
+     * The path to the fixture files. By default, this is `{root}/database/fixtures`.
+     */
     'path' => env('FROM_FIXTURE_PATH', database_path('fixtures')),
 
+    /**
+     * The namespace for the models in your app. If you have a Models directory,
+     * it will be \App\Models, otherwise just \App.
+     */
     'models' => [
         'namespace' => env('FROM_FIXTURE_MODEL_NAMESPACE', is_dir(app_path('Models')) ? 'App\\Models\\' : 'App\\'),
     ],
